e325575fcc557003315ceacaf641831ebd436c0c
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-02-13 23:20:28 +0000

    update

--- Portail/settings.py
+++ Portail/settings.py
@@ -94,19 +94,19 @@ WSGI_APPLICATION = 'Portail.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portail_db',
-        'USER': 'postgres',
-        'PASSWORD': 'testing321',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'portail_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'testing321',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 PASSWORD_HASHERS = [
--- authentication/models.py
+++ authentication/models.py
@@ -36,28 +36,29 @@ class Admin(models.Model):
         (ADMIN, 'ADMIN')
     )
 
-    user = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="ADMIN")
     password = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     number = PhoneNumberField(null=False, blank=False)
 
     def __str__(self):
-        return self.user
+        return self.first_name
     
 
     @receiver(post_save, sender="authentication.Admin")
     def addGroup(sender, instance, created, **kwargs):
         if created:
             if instance.role == "ADMIN":
-                User.objects.create_user(username=instance.user, role=instance.role, password=instance.password, email=instance.email, is_staff=True)
+                User.objects.create_user(username=instance.first_name, role=instance.role, password=instance.password, email=instance.email, is_staff=True)
                 admin_permission = Permission.objects.filter(name__contains="view admin")
                 comptabilite_permission = Permission.objects.filter(name__contains="comptabilite")
                 direction_permission = Permission.objects.filter(name__contains="direction")
                 magasin_permission = Permission.objects.get(name="Can view magasin")
                 magboard_permission = Permission.objects.get(name="Can view magasin_value")
                 superviseur_permission = Permission.objects.get(name="Can view superviseur")
-                user = User.objects.get(username=instance.user)
+                user = User.objects.get(username=instance.first_name)
                 user.user_permissions.add(*admin_permission)
                 user.user_permissions.add(*comptabilite_permission)
                 user.user_permissions.add(*direction_permission)
@@ -67,8 +68,8 @@ class Admin(models.Model):
     @receiver(pre_delete, sender="authentication.Admin")
     def delete_user(sender, instance, **kwargs):
        
-            if User.objects.filter(username=instance.user, role=instance.role).exists():
-                user = User.objects.get(username=instance.user, role=instance.role)
+            if User.objects.filter(username=instance.first_name, role=instance.role).exists():
+                user = User.objects.get(username=instance.first_name, role=instance.role)
                 user.delete()
 
            
@@ -78,25 +79,26 @@ class Direction(models.Model):
         ("", '----'),
         (DIRECTION, 'DIRECTION')
     )
-    user = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="DIRECTION")
     password = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     number = PhoneNumberField(null=False, blank=False)
     def __str__(self):
-        return self.user
+        return self.first_name
 
     @receiver(post_save, sender="authentication.Direction")
     def addGroup(sender, instance, created, **kwargs):
         if created:
             if instance.role == "DIRECTION":
-                User.objects.create_user(username=instance.user, role=instance.role, email=instance.email, password=instance.password)
+                User.objects.create_user(username=instance.first_name, role=instance.role, email=instance.email, password=instance.password)
 
     @receiver(pre_delete, sender="authentication.Direction")
     def delete_user(sender, instance, **kwargs):
        
-            if User.objects.filter(username=instance.user, role=instance.role).exists():
-                user = User.objects.get(username=instance.user, role=instance.role)
+            if User.objects.filter(username=instance.first_name, role=instance.role).exists():
+                user = User.objects.get(username=instance.first_name, role=instance.role)
                 user.delete()
 
 
@@ -106,25 +108,26 @@ class Comptabilite(models.Model):
         ("", '----'),
         (COMPTABILITE, 'COMPTABILITE')
     )
-    user = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="COMPTABILITE")
     password = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     number = PhoneNumberField(null=False, blank=False)
     def __str__(self):
-        return self.user
+        return self.first_name
 
     @receiver(post_save, sender="authentication.Comptabilite")
     def addGroup(sender, instance, created, **kwargs):
         if created:
             if instance.role == "COMPTABILITE":
-                User.objects.create_user(username=instance.user, role=instance.role, email=instance.email, password=instance.password)
+                User.objects.create_user(username=instance.first_name, role=instance.role, email=instance.email, password=instance.password)
     
     @receiver(pre_delete, sender="authentication.Comptabilite")
     def delete_user(sender, instance, **kwargs):
        
-            if User.objects.filter(username=instance.user, role=instance.role).exists():
-                user = User.objects.get(username=instance.user, role=instance.role)
+            if User.objects.filter(username=instance.first_name, role=instance.role).exists():
+                user = User.objects.get(username=instance.first_name, role=instance.role)
                 user.delete()
 
                 
@@ -174,7 +177,8 @@ class Superviseur(models.Model):
         ("", '----'),
         (SUPERVISEUR, 'SUPERVISEUR')
     )
-    user = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="SUPERVISEUR")
     magasin = models.ManyToManyField(Magasin, related_name="Magasins", blank=True)
@@ -184,19 +188,19 @@ class Superviseur(models.Model):
         super().__init__(*args, **kwargs)
 
     def __str__(self):
-        return self.user
+        return self.first_name
 
     @receiver(post_save, sender="authentication.Superviseur")
     def addGroup(sender, instance, created, **kwargs):
         if created:
             if instance.role == "SUPERVISEUR":
-                User.objects.create_user(username=instance.user, role=instance.role, email=instance.email, password=instance.password)
+                User.objects.create_user(username=instance.first_name, role=instance.role, email=instance.email, password=instance.password)
 
     @receiver(pre_delete, sender="authentication.Superviseur")
     def delete_user(sender, instance, **kwargs):
        
-            if User.objects.filter(username=instance.user, role=instance.role).exists():
-                user = User.objects.get(username=instance.user, role=instance.role)
+            if User.objects.filter(username=instance.first_name, role=instance.role).exists():
+                user = User.objects.get(username=instance.first_name, role=instance.role)
                 user.delete()
 
 
@@ -206,7 +210,8 @@ class RH(models.Model):
         ("", '----'),
         (RH, 'RH')
     )
-    user = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default="RH")
     
@@ -216,17 +221,17 @@ class RH(models.Model):
         super().__init__(*args, **kwargs)
 
     def __str__(self):
-        return self.user
+        return self.first_name
 
     @receiver(post_save, sender="authentication.RH")
     def addGroup(sender, instance, created, **kwargs):
         if created:
             if instance.role == "RH":
-                User.objects.create_user(username=instance.user, role=instance.role, email=instance.email, password=instance.password)
+                User.objects.create_user(username=instance.first_name, role=instance.role, email=instance.email, password=instance.password)
 
     @receiver(pre_delete, sender="authentication.RH")
     def delete_user(sender, instance, **kwargs):
        
-            if User.objects.filter(username=instance.user, role=instance.role).exists():
-                user = User.objects.get(username=instance.user, role=instance.role)
+            if User.objects.filter(username=instance.first_name, role=instance.role).exists():
+                user = User.objects.get(username=instance.first_name, role=instance.role)
                 user.delete()
